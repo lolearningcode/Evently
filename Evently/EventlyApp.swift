30c00622950889a690f830b0a1a4fb21eb81db99
iosdev.cleo <iosdev.cleo@gmail.com>
2022-01-12 06:01:42 -0800

    Initial commit

---
+++ Evently/EventlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventlyApp.swift
+//  Evently
+//
+//  Created by Cleo Howard on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct EventlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
