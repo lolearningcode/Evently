4ca53ba94711ea5391d87dee416c860fa15e523e
iosdev.cleo <iosdev.cleo@gmail.com>
2022-01-17 13:33:54 -0800

    added func for getting weather

--- Evently/Utilities/NetworkManager.swift
+++ Evently/Utilities/NetworkManager.swift
@@ -10,16 +10,17 @@ import UIKit
 import Combine
 
 protocol APIService {
-    func request(from endpoint: NewsAPIEndpoints) -> AnyPublisher<NewsResponse, APIError>
-//    func downloadImage(from endpoint: NewsAPIEndpoints) -> UIImage
+    func request(from endpoint: APIEndpoints) -> AnyPublisher<NewsResponse, APIError>
+    func requestWeather(from endPoint: APIEndpoints) -> AnyPublisher<WeatherResponse, APIError>
 }
 
 struct NewsService: APIService {
-    func request(from endpoint: NewsAPIEndpoints) -> AnyPublisher<NewsResponse, APIError> {
+    
+    func request(from endpoint: APIEndpoints) -> AnyPublisher<NewsResponse, APIError> {
         return URLSession.shared
             .dataTaskPublisher(for: endpoint.urlRequest)
             .receive(on: DispatchQueue.main)
-            .mapError { _ in APIError.unknown }
+            .mapError { _ in APIError.invalidURL }
             .flatMap { data, response -> AnyPublisher<NewsResponse, APIError> in
                 guard let response = response as? HTTPURLResponse else {
                     return Fail(error: APIError.invalidResponse).eraseToAnyPublisher()
@@ -31,6 +32,31 @@ struct NewsService: APIService {
                     
                     return Just(data)
                         .decode(type: NewsResponse.self, decoder: decoder)
+                        .mapError { _ in APIError.decodingData }
+                        .eraseToAnyPublisher()
+                } else {
+                    return Fail(error: APIError.invalidResponse).eraseToAnyPublisher()
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func requestWeather(from endPoint: APIEndpoints) -> AnyPublisher<WeatherResponse, APIError> {
+        return URLSession.shared
+            .dataTaskPublisher(for: endPoint.urlRequest)
+            .receive(on: DispatchQueue.main)
+            .mapError { _ in APIError.invalidURL }
+            .flatMap { data, response -> AnyPublisher<WeatherResponse, APIError> in
+                guard let response = response as? HTTPURLResponse else {
+                    return Fail(error: APIError.invalidResponse).eraseToAnyPublisher()
+                }
+                
+                if (200...299).contains(response.statusCode) {
+                    let decoder = JSONDecoder()
+                    decoder.dateDecodingStrategy = .iso8601
+                    
+                    return Just(data)
+                        .decode(type: WeatherResponse.self, decoder: decoder)
                         .mapError { _ in APIError.decodingData }
                         .eraseToAnyPublisher()
                 } else {
