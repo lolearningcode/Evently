4f72c739e3ce370d21301aa442f8b98acaa855b6
iosdev.cleo <iosdev.cleo@gmail.com>
2022-01-17 21:43:22 -0800

    refactored image view

--- Evently/ArticleImageView.swift
+++ Evently/ArticleImageView.swift
@@ -11,34 +11,40 @@ import URLImage
 struct ArticleImageView: View {
     
     let article: Article
-    let width: CGFloat
-    let height: CGFloat
     
     var body: some View {
         if let imageURL = article.image,
-            let url = URL(string: imageURL) {
+           let url = URL(string: imageURL) {
             
             URLImage(url, failure: { error, _ in
                 PlaceHolderImageView()
             }) { image in
                 image
                     .resizable()
-                    .frame(width: width, height: height)
+                    .scaledToFill()
+                    .frame(height: 250)
                     .clipped()
             }
             
             .environment(\.urlImageOptions, URLImageOptions(fetchPolicy: .returnStoreElseLoad(downloadDelay: 0.25), loadOptions: .loadOnAppear))
         } else {
-           PlaceHolderImageView()
+            PlaceHolderImageView()
+        }
+        
+        VStack(alignment: .leading) {
+            Text(article.title)
+                .font(.caption)
+                .fontWeight(.bold)
+            
+            Text("\(article.date)")
+                .font(.caption2)
         }
     }
 }
 
 struct ArticleImageView_Previews: PreviewProvider {
     static var previews: some View {
-        ArticleImageView(article: Article.dummyData,
-                         width: 250,
-                         height: 250)
+        ArticleImageView(article: Article.dummyData)
     }
 }
 
